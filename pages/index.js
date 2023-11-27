19b81115e819c9637e7d45100517af9e95a9fee2
cristiano-dsgn <cristiano.c7tm@gmail.com>
2023-11-27 17:49:27 +0000

    mensagem para meu pai

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Muito certo.</h1>
+    return <h1>Obrigado por tudo. Sei o quanto o senhor se esforça, cada vez, mais para nos acompanhar, está do nosso lado, e mas que ser um pai, ser um amigo. Tenho muito orgulho do senhor e de ser seu filho. Obrigado, Pai.</h1>
 }
 
 export default Home;
